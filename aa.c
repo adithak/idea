3dd74209097d22c5eb661249964d0ce87d9201f4
adithak <adithak59@gmail.com>
2019-01-15 23:43:02 +0530

    Create aa.c

---
+++ aa.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,total;
+clrscr();
+printf(" enter the values of a");
+scanf("%d"&a);
+printf(" enter the value of b");
+scanf("%d",&b);
+printf("enter the value of c");
+scanf("%d",&c);
+total=a+(b-1)c;
+Printf("the value is",&total);
+getch();
+}
