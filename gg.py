27a212ca110c3874d71b997655e40b80647f364b
adithak <adithak59@gmail.com>
2019-01-18 14:34:42 +0530

    Create gg.py

---
+++ gg.py
@@ -0,0 +1,9 @@
+alphabet = input("Enter the string")
+data = alphabet.split() #split string into a list
+n=len(data)
+for i in range(n):
+	if i%2==0:
+		temp=data[i]
+		print(temp[::-1])
+	else:
+		print(data[i])
