604930e17e80500845514ddcab1f59670787c6de
adithak <adithak59@gmail.com>
2019-01-18 14:47:46 +0530

    Create jj.java

---
+++ jj.java
@@ -0,0 +1,16 @@
+package pro;
+import java.util.*;
+public class pro1_5 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		N/=2;
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int x=0,y=a+b;
+		if(N%y==0){
+			System.out.println("YES");
+	}
+	}
+}
